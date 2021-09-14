462a7a736718a43b8ef072e5f691fb60fbd431fb
ROBERTO HAHN BRUM <82846015+robertohbr1@users.noreply.github.com>
2021-09-14 12:46:28 -0300

    Commit 2

---
+++ src/containers/Home.jsx
@@ -0,0 +1,56 @@
+import React,{ memo, useEffect, useState } from "react";
+import {Row, Col} from 'antd'
+import Api from '../api'
+import Economy from './components/Economy'
+import Technology from './components/Technology'
+import World from './components/World'
+
+function Home() {
+    const [news, setNews] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    const hadleNews = (articles) => {
+        setLoading(false)
+        setNews({
+            world: articles[0]?.value.value,
+            economy: articles[1]?.value.value,
+            technology: articles[2]?.value.value,
+        })
+    }
+
+    useEffect(() => {
+        setLoading(true)
+        Promise.allSettled([
+            Api.getNews('world'),
+            Api.getNews('economy'),
+            Api.getNews('technology')
+        ])
+        .then(hadleNews)
+    }, [])
+
+    if (loading)  return <div>Carregando</div>
+
+    return (
+    <div>
+        <Row gutter={[16, 16]}>
+            <Col span={24} md={16}>
+                <h2>World</h2>
+                <World values={news?.world} />
+            </Col>
+            <Col span={24} md={8}>
+                <h2>Economy</h2>
+                <Economy values={news?.economy} />
+            </Col>
+        </Row>
+        <hr />
+        <Row gutter={[16, 16]}>
+            <Col span={24}>
+                <h2>Technology</h2>
+                <Technology values={news?.technology} />
+            </Col>
+        </Row>
+    </div>
+    );
+}
+
+export default memo(Home)
